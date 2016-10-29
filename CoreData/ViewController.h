701e537e0db68616f1399c910155554e5338dfbf
EnzoFurmenti <ducheenzo@mail.ru>
2016-10-29 10:32:28 +0300

    Initial Commit

---
+++ CoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreData
+//
+//  Created by EnzoF on 29.10.16.
+//  Copyright © 2016 EnzoF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
